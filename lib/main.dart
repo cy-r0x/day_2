907227d6856245c79bddad8e999f44079633fb49
cy-r0x <payel15-5106@diu.edu.bd>
2025-02-18 09:54:05 +0600

    Flutter_Row Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,43 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Basic UI - 02",
-        home: Scaffold(
-            appBar: AppBar(
+      title: "Basic UI - 02",
+      home: Scaffold(
+        appBar: AppBar(
           title: Text("Basic UI - 02"),
           backgroundColor: Colors.blue,
-        )));
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              "Hi",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              "This is",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              "Flutter",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
