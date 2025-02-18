58ebea4afe3a5590d4e8b3f492eae5bcbfdb5d2d
cy-r0x <payel15-5106@diu.edu.bd>
2025-02-18 12:55:59 +0600

    Lab P2

--- lib/main.dart
+++ lib/main.dart
@@ -19,32 +19,79 @@ class App extends StatelessWidget {
             fontSize: 20,
           ),
         ),
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Rating",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 244, 244, 244),
+                borderRadius: BorderRadius.circular(30),
               ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(
-                    100,
-                    (index) => Icon(
-                      Icons.star,
-                      size: 1.00 * index + 1,
-                      color: Colors.cyan,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text(
+                          "50% OFF",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.local_hospital_rounded,
+                        size: 40,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Image.network(
+                      "https://nikearprod.vtexassets.com/arquivos/ids/1217812-800-800?width=800&height=800&aspect=true",
                     ),
                   ),
-                ),
+                  Text(
+                    "Nike Air Max",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "\$45,000",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                        Text(
+                          "\$50,000",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
